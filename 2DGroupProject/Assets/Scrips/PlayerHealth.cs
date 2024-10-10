bd36c3ccbbaceed1488b20801d094c681d1b9068
Mixmaximum <mixmaximum752@gmail.com>
2024-10-10 10:25:53 -0700

    Fixed Coyote Time, and finished Fall damage

--- 2DGroupProject/Assets/Scrips/PlayerHealth.cs
+++ 2DGroupProject/Assets/Scrips/PlayerHealth.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField]
-    int health = 10;
+    public int health = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,7 @@ public class PlayerHealth : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
+        //Debug.Log(collision.gameObject.name);
         //we want to take damage IF the player hits the enemy capsule
         //bool key = true;
         if (collision.gameObject.tag == "Enemy")
@@ -27,6 +28,11 @@ public class PlayerHealth : MonoBehaviour
             //health = health - 1;
             health -= 1;
             //health--;
+            if (health <= 0)
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+            }
         }
     }
     private void OnCollisionStay2D(Collision2D collision)
@@ -34,6 +40,11 @@ public class PlayerHealth : MonoBehaviour
         if (collision.gameObject.tag == "Enemy")
         {
             health -= 1;
+            if (health <= 0)
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+            }
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
